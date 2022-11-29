cbf41f0a5a1a5ab77670e77e9cc679a144b145ae
Brayan Rai <brayanrai644@gmail.com>
2022-11-30 01:43:47 +0530

    add file for problem 1 watermelon from codeforces

---
+++ watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int weight;
+    cin>>weight;
+    if( weight%2 == 0 and weight!=2)
+    {
+    cout<< "Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+    return 0;
+}
